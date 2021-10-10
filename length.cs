eb5278de5c11738a973aaf077b1d866ecfd38188
jailor123 <91145870+jailor123@users.noreply.github.com>
2021-10-10 18:02:13 +0530

    length.cs

---
+++ length.cs
@@ -0,0 +1,24 @@
+using System;
+ 
+class line {
+
+    static void main(int x, int y)
+    {
+        if  (x1  > 0 & y1 > 0)
+        else if (x2 > 0 &   y2 > 0)
+      }
+        
+      
+        
+        
+        public static void Main()
+    {
+        int x1 = 1, x2= 1, x3=2,x3=4;
+         Length of a Line = sqrt( (x2 - x1) ^ 2 + (y2- y1) ^ 2)
+      }
+         
+         
+         
+            
+ 
+ 
